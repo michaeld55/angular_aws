ff5dc0e1754ba5e542af103ea42dbc853a98bebd
Michael <michaeld55@hotmail.co.uk>
2021-01-08 14:00:28 +0000

    added delete, starting edit

---
+++ src/types/restaurant.ts
@@ -0,0 +1,8 @@
+import { ExecSyncOptionsWithBufferEncoding } from "child_process";
+
+export type Restaurant = {
+    id: string,
+    name : string,
+    description : string,
+    city : string
+};
